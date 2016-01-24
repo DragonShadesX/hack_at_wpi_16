cfc134e53264968537f8adc39055530927690380
GranTotem <aefernandez@wpi.edu>
2016-01-24 02:43:33 -0500

    Final Version
    Final code with comments and fixed variable names.

--- canned_responses.lua
+++ canned_responses.lua
@@ -1,3 +1,17 @@
+--[[
+
+23/1/16
+#Version: 1.0
+#Authors: Greg Tighe (DragonShadesX), Alan Fernandez (GranTotem)
+#Hardware: Myo Armband V1
+
+#Our entry for the WPI Hackathon 2016. The script allows the user to enter predefined responses into any text field using gestures recognized by the myo armband (Fist, Wave In, Wave Out and Spread Fingers)
+#The accompanying Python script creates a GUI where the user can enter the desired canned responses. The script then edits the .lua file and modifies lines 68-71 which are the strings containing these responses.
+#Scripts that modify scripts sound like viruses, but this is a hackathon and Myo doesn't allow scripts to interact directly with the filesystem.
+
+#NOTE: If the number of lines changes, THE PYTHON SCRIPT WILL NO LONGER WORK! You must edit the lines in the python script. 
+]]--
+
 scriptId = ''
 scriptDetailsUrl = ''
 scriptTitle = 'Canned Responses'
@@ -8,7 +22,7 @@ function onForegroundWindowChange(app, title)
 end
 
 function activeAppName()
-    return "Any"
+    return "Canned Responses"
 end
 
 -- flag to de/activate shuttling feature
@@ -37,31 +51,33 @@ function shuttleBurst()
     end
 end
 
--- Triggers
+--
 function magic(arrayToPrint)
     --Magic begins here
-                local message_array = {}
-                arrayToPrint:gsub(".", function(c) table.insert(message_array,c) end)
-				count = 1
-				for Index, Value in pairs(message_array) do
-					count = count + 1
-				end
-                local i = 1
-                while (i<count) do
-                    if message_array[i] == " " then message_array[i] = "space" end
-                    myo.keyboard(message_array[i], "press")
-                i = i + 1
-                end
-                --No more magic
+    local message_array = {}
+    arrayToPrint:gsub(".", function(c) table.insert(message_array,c) end)
+    count = 1
+	for Index, Value in pairs(message_array) do
+	    count = count + 1
+	end
+    local i = 1
+    while (i<count) do
+        if message_array[i] == " " then message_array[i] = "space" end
+        myo.keyboard(message_array[i], "press")
+        i = i + 1
+        end
+    myo.keyboard("return", "press")
+    --No more magic
 end
-waveInGestureString = "waveingesture"
-waveInGestureString = "1"
-waveOutGestureString = "2"
-fistGestureString = "3"
-openHandGestureString = "4"
+
+--Strings containing the user defined responses, these get edited by the python script
+waveInGestureString = "wave in gesture"
+waveOutGestureString = "wave out gesture"
+fistGestureString = "fist gesture"
+fingerSpreadGestureString = "open hand gesture"
 
 function onPoseEdge(pose, edge)
-    -- Forward/backward and shuttle
+    --Wave Gestures
     if pose == "waveIn" or pose == "waveOut" then
         local now = myo.getTimeMilliseconds()
 
@@ -88,13 +104,17 @@ function onPoseEdge(pose, edge)
             shuttleTimeout = nil
         end
     end
+
+    --Open Hand Gesture
     if pose == "fingersSpread" then
         if edge == "on" then
             if pose  == "fingersSpread" then
-                magic(openHandGestureString)
+                magic(fingerSpreadGestureString)
             end
         end
     end
+
+    --Fist Gesture
     if pose == "fist" then
         if edge == "on" then
             if pose  == "fist" then
